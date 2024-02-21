183f634f0ac74dcaa29f4c405233182f3b748325
Gabriel Prestes <darkprestes10@gmail.com>
2024-02-21 12:00:21 -0300

    SellerForm

--- src/gui/SellertListController.java
+++ src/gui/SellertListController.java
@@ -10,14 +10,19 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Seller;
 import model.services.SellerService;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -93,7 +98,6 @@ public class SellertListController implements Initializable, DataChangeListener 
 
 
     private void createDialogForm(Seller obj, String absoluteName, Stage parentStage) {
-       /*
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
             Pane pane = loader.load();
@@ -105,7 +109,7 @@ public class SellertListController implements Initializable, DataChangeListener 
             controller.updateFormData();
 
             Stage dialogStage = new Stage();
-            dialogStage.setTitle("Enter department data");
+            dialogStage.setTitle("Enter seller data");
             dialogStage.setScene(new Scene(pane));
             dialogStage.setResizable(false);
             dialogStage.initOwner(parentStage);
@@ -115,7 +119,6 @@ public class SellertListController implements Initializable, DataChangeListener 
         catch (IOException e){
             Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
         }
-          */
     }
 
 
